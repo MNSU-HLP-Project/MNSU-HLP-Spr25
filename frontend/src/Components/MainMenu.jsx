8419adca0112b17d4031b09e46068cd9b59c9b3d
kolemanl <kolemanlund@gmail.com>
2025-02-17 21:22:22 -0600

    Update MainMenu.jsx

--- frontend/src/Components/MainMenu.jsx
+++ frontend/src/Components/MainMenu.jsx
@@ -11,10 +11,15 @@ const MainMenu = () => {
   }
 
   return (
-    <button className="w-full mt-4 p-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-    onClick={buttonPress}>
-        Log Out
-    </button>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
+      <div className="w-full max-w-md bg-white shadow-md rounded-lg p-6">
+        <button className="w-full mt-4 p-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+        onClick={buttonPress}>
+            Log Out
+        </button>
+      </div>
+      </div>
+    
   );
 };
 
