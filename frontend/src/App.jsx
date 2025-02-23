bf7332328c7434d51ed7d970704fc07d98bbb01c
kolemanl <kolemanlund@gmail.com>
2025-02-23 11:56:23 -0600

    Main Menu Updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
 import Auth from "./Components/Auth"; // Updated import to the new component
 import MainMenu from "./Components/MainMenu";
+import HLPCategories from "./Components/HLPCategories";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/mainmenu/" element={<MainMenu />} />
+      <Route path='/hlpcategories/' element={<HLPCategories />} />
     </Routes>
   );
 }
--- frontend/src/Components/MainMenu.jsx
+++ frontend/src/Components/MainMenu.jsx
@@ -3,10 +3,9 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const MainMenu = () => {
-  const [open, setOpen] = useState(false);
   const navigate = useNavigate();
-  const buttonPress = () => {
-    navigate("/");
+  const buttonPress = (route) => {
+    navigate(route);
   };
 
   return (
@@ -14,7 +13,13 @@ const MainMenu = () => {
       <div className="w-full max-w-md bg-white shadow-md rounded-lg p-6">
         <button
           className="w-full mt-4 p-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-          onClick={buttonPress}
+          onClick={() => buttonPress('/hlpcategories/')}
+        >
+          HLP Categories
+        </button>
+        <button
+          className="w-full mt-4 p-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+          onClick={() => buttonPress('/')}
         >
           Log Out
         </button>
