45de9c809f29e8468ccb4e33f0bae9931e7f0808
Emmanuel Kiprotich <155409053+EGKIP@users.noreply.github.com>
2025-04-11 00:07:11 -0500

    HLP submission flow and error handling - 2

--- frontend/src/dashboard/supervisor/SupervisorMainView.jsx
+++ frontend/src/dashboard/supervisor/SupervisorMainView.jsx
@@ -13,10 +13,10 @@ const SupervisorMainView = () => {
   return (
     <>
       <button
-        className="w-3/4 p-4 md:p-5 border-2 border-blue-700 text-white bg-blue-700 rounded-lg hover:bg-blue-800 flex items-center justify-center shadow-lg transition duration-300 transform hover:scale-105 font-semibold text-xl md:text-2xl"
-        onClick={() => navigate("/classes")}
+        className="w-3/4 p-4 md:p-5 border-2 border-purple-700 text-white bg-purple-700 rounded-lg hover:bg-purple-800 flex items-center justify-center shadow-lg transition duration-300 transform hover:scale-105 font-semibold text-xl md:text-2xl"
+        onClick={() => navigate("/supervisor/review/")}
       >
-        View Reflections
+        📝 Review Student Reflections
       </button>
 
       {/* Invite Link Section */}
