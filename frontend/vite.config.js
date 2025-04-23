9242d79023043f5c93ece7815092c21245f17774
kolemanl <kolemanlund@gmail.com>
2025-04-23 15:04:23 -0500

    Dev On Phone

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { qrcode } from 'vite-plugin-qrcode';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),qrcode()],
+  server: {
+    host: true,  // Allows LAN access
+    port: 5173,  // Default port
+  },
   css: {
     postcss: './postcss.config.js',
   },
